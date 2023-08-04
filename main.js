173e4daba4980a5cfa8702e100513180fe2356ab
namle89 <lehoangnam89@gail.com>
2023-08-05 02:47:43 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function to_open_TX()
+{
+    window.location.href = "/TX.html"
+}
+
+function to_open_CA()
+{
+    window.location.href = "/CA.html"
+}
+
+function closeWebsite() {
+    window.close();
+}
